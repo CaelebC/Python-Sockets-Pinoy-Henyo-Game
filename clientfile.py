99073c48c1feb73f5d64270d16113d118320f23a
CaelebC <caelebcamaro1@gmail.com>
2022-10-31 22:30:06 +0800

    Used 'with' statements to shorten code. Client input now works with selection function

--- clientfile.py
+++ clientfile.py
@@ -1,37 +1,42 @@
 import socket
 
-HEADER = 64
-# HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
-SERVER = "192.168.56.1"
-ADDR = (SERVER, PORT)
+PORT = 65432
+CLIENT = socket.gethostbyname(socket.gethostname())
+ADDR = (CLIENT, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "/dc"
+HEADER = 64
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client.connect(ADDR)
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
+# def send(msg):
+#     message = msg.encode(FORMAT)
+#     msg_length = len(message)
+#     send_length = str(msg_length).encode(FORMAT)
+#     send_length += b' ' * (HEADER - len(send_length))
     
-    client.send(send_length)
-    client.send(message)
+#     client.send(send_length)
+#     client.send(message)
 
-    print(client.recv(2048).decode(FORMAT))
+#     print(client.recv(2048).decode(FORMAT))
 
-while True:
-    user_input = input()
-    if user_input == DISCONNECT_MESSAGE:
-        break
-    send(user_input)
+# while True:
+#     user_input = input()
+#     if user_input == DISCONNECT_MESSAGE:
+#         break
+#     send(user_input)
 
 
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.connect((HOST, PORT))
-#     s.sendall(b"Hello, world")
-#     data = s.recv(1024)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+    client.connect((CLIENT, PORT))
+    while True:
+        user_input = str(input())
+        if user_input == DISCONNECT_MESSAGE:
+            break
+        data_to_send = user_input.encode(FORMAT)
+        client.send(data_to_send)
 
-# print(f"Received {data!r}")
+        server_data = client.recv(2048)
+
+# print(f"Received {server_data!r}")
--- serverfile.py
+++ serverfile.py
@@ -1,73 +1,50 @@
-import socket
-import threading
+# References:
+# https://www.youtube.com/watch?v=3QiPPX-KeSc
+# https://realpython.com/python-sockets/
 
-HEADER = 64
-# HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+import socket
+import random
+
+PORT = 65432
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "/dc"
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
-
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected")
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-                print(f"[DISCONNECTED USER] {addr}")
-                break
-            print(f"[{addr}] {msg}")
-            conn.send("[MSG RECEIVED]".encode(FORMAT))
-    conn.close()
-
-def start():
-    print("[SERVER IS STARTING]")
-    server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}")
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS]: {threading.activeCount() - 1}")
-
+HEADER = 64
 
 # PINOY HENYO GAME SECTION
-import random
-
+# Proper nouns are set to lower case for simplicity in the code
 WORDSET = [
     'farm', 'building', 'resorts world manila', 'stadium', 'tower',
     'sisig', 'adobo', 'fried chicken', 'pizza', 'noodles', 'sushi',
     'denji', 'anya', 'jose rizal', 'juan', 'juana', 'ina', 'pomu',
     'penguin', 'fish', 'cat', 'dog', 'octopus', 'bird', 'dragon',
 ]
-# Proper nouns are set to lower case for simplicity in the code
 ATTEMPTS = 25
 
 def pinoy_henyo_select():
-    selection = str(input('Use random word from wordset? Type Y or N\n')).upper()
+    # selection = str(input('Use random word from wordset? Type Y or N\n')).upper()
+    print('Use random word from wordset? Type Y or N\n')
+    selection = ((conn.recv(2048)).decode(FORMAT)).upper()
+
     if selection == 'Y':
         word = random.choice(WORDSET)
         pinoy_henyo_game(word, ATTEMPTS)
 
     else:
+        # TODO: IMPLEMENT A CUSTOM SERVER INPUT FOR CLIENT TO GUESS
         pass
 
 def pinoy_henyo_game(_word, _attempts):
     # why does python not have a do while loop this is the only time i want it
-    print(f'[GAME START] TOTAL ATTEMPTS: {_attempts}')
+    send_to_client(f'[GAME START] TOTAL ATTEMPTS: {_attempts}')
     _word = _word
     user_attempts = 0
     failed = True
     while user_attempts < _attempts:
-        user_input = str(input()).lower()
+        # user_input = str(input()).lower()
+        user_input = ((conn.recv(2048)).decode(FORMAT)).upper()
         user_attempts += 1
         print('\n')
 
@@ -77,28 +54,57 @@ def pinoy_henyo_game(_word, _attempts):
             failed = False
             break
         else:
-            print('well you are not correct, try again')
-            print(f'[ATTEMPTS REMAINING] {(_attempts - user_attempts)}')
+            send_to_client('well you are not correct, try again')
+            send_to_client(f'[ATTEMPTS REMAINING] {(_attempts - user_attempts)}')
             continue
     
     if failed:
-        print('The correct word was: ' + _word)
+        send_to_client('The correct word was: ' + _word)
 
+# Socket Section -- Server
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.bind((SERVER, PORT))
+    print("[SERVER IS STARTING]")
+    server.listen()
+    print(f"[LISTENING] on {SERVER}")
+    conn, addr = server.accept()
+    
+    def send_to_client(text):
+        data_to_send = text.encode(FORMAT)
+        server.send(data_to_send)
+        print('[SERVER] ' + text)
 
-pinoy_henyo_select()
+    with conn:
+        print(f"[NEW CONNECTION] {addr} connected")
+        pinoy_henyo_select()
+        
 
+        # while True:
+        #     msg = conn.recv(2048)
+        #     if not msg:
+        #         break
 
+            
+        #     conn.sendall(b'[DATA RECEIVED]')
+        
+        # connected = True
+        # while connected:
+        #     msg_length = conn.recv(HEADER).decode(FORMAT)
+        #     if msg_length:
+        #         msg_length = int(msg_length)
+        #         msg = conn.recv(msg_length).decode(FORMAT)
+        #         if msg == DISCONNECT_MESSAGE:
+        #             connected = False
+        #             print(f"[DISCONNECTED USER] {addr}")
+        #             break
+        #         print(f"[{addr}] {msg}")
+        #         conn.send("[MSG RECEIVED]".encode(FORMAT))
+        # conn.close()
 
-
-
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.bind((HOST, PORT))
-#     s.listen()
-#     conn, addr = s.accept()
-#     with conn:
-#         print(f"Connected by {addr}")
-#         while True:
-#             data = conn.recv(1024)
-#             if not data:
-#                 break
-#             conn.sendall(data)
+    # This was part of the video tutorial, which allows something to run wihtout 
+    # having to wait for another process. For our purposes this is not needed,
+    # but might be helpful for other added features down the line.
+    # 
+    # thread = threading.Thread(target=handle_client, args=(conn, addr))
+    # thread.start()
+    # print(f"[ACTIVE CONNECTIONS]: {threading.activeCount() - 1}") 
