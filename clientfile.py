22cdf3f375d492f320724e1eca1cbd33f092b7b9
CaelebC <caelebcamaro1@gmail.com>
2022-10-31 23:02:19 +0800

    Basic game functionality works

--- clientfile.py
+++ clientfile.py
@@ -1,3 +1,8 @@
+# References:
+# https://www.youtube.com/watch?v=3QiPPX-KeSc
+# https://realpython.com/python-sockets/
+
+
 import socket
 
 PORT = 65432
@@ -7,36 +12,19 @@ FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "/dc"
 HEADER = 64
 
-# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# client.connect(ADDR)
-
-# def send(msg):
-#     message = msg.encode(FORMAT)
-#     msg_length = len(message)
-#     send_length = str(msg_length).encode(FORMAT)
-#     send_length += b' ' * (HEADER - len(send_length))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clie:
+    clie.connect((CLIENT, PORT))
+    print( clie.recv(2048).decode(FORMAT) )   # Server will send 2 messages
+    print( clie.recv(2048).decode(FORMAT) )   # this is to display it in client
     
-#     client.send(send_length)
-#     client.send(message)
-
-#     print(client.recv(2048).decode(FORMAT))
-
-# while True:
-#     user_input = input()
-#     if user_input == DISCONNECT_MESSAGE:
-#         break
-#     send(user_input)
-
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-    client.connect((CLIENT, PORT))
     while True:
+        # BUG: When user sends a blank string (i.e. ''), then both terminals of
+        # server and client freeze up.
+        # BUG: When server closes itself, the client still needs to input anything
+        # in the terminal to close the program.
         user_input = str(input())
         if user_input == DISCONNECT_MESSAGE:
             break
-        data_to_send = user_input.encode(FORMAT)
-        client.send(data_to_send)
+        clie.send( user_input.encode(FORMAT) )
 
-        server_data = client.recv(2048)
-
-# print(f"Received {server_data!r}")
+        print( clie.recv(2048).decode(FORMAT) )
--- serverfile.py
+++ serverfile.py
@@ -24,87 +24,57 @@ WORDSET = [
 ATTEMPTS = 25
 
 def pinoy_henyo_select():
-    # selection = str(input('Use random word from wordset? Type Y or N\n')).upper()
-    print('Use random word from wordset? Type Y or N\n')
+    print_serv_clie('Use random word from wordset? Type Y or N\n')
     selection = ((conn.recv(2048)).decode(FORMAT)).upper()
 
     if selection == 'Y':
         word = random.choice(WORDSET)
+        print('THE CHOSEN WORD IS: ' + word)
         pinoy_henyo_game(word, ATTEMPTS)
 
     else:
         # TODO: IMPLEMENT A CUSTOM SERVER INPUT FOR CLIENT TO GUESS
-        pass
+        print('THE CHOSEN WORD IS: ' + 'uh this isnt working yet why are you here dont use this yet')
 
 def pinoy_henyo_game(_word, _attempts):
-    # why does python not have a do while loop this is the only time i want it
-    send_to_client(f'[GAME START] TOTAL ATTEMPTS: {_attempts}')
+    # why does python not have a do-while loop this is the only time i want it
+    print_serv_clie(f'[GAME START] TOTAL ATTEMPTS: {_attempts}')
     _word = _word
     user_attempts = 0
     failed = True
     while user_attempts < _attempts:
-        # user_input = str(input()).lower()
-        user_input = ((conn.recv(2048)).decode(FORMAT)).upper()
+        user_input = ((conn.recv(2048)).decode(FORMAT)).lower()
         user_attempts += 1
-        print('\n')
 
         if user_input == _word:
-            print('CONGRATULATIONS! YOU GOT THE WORD')
-            print(f'It took you {user_attempts} to get it right')
+            print_serv_clie(f'\nCONGRATULATIONS YOU GOT THE WORD!\nIt took you {user_attempts} attempts to get it right.')
             failed = False
             break
         else:
-            send_to_client('well you are not correct, try again')
-            send_to_client(f'[ATTEMPTS REMAINING] {(_attempts - user_attempts)}')
-            continue
+            # TODO: ALLOW SERVER TO 'REPLY' WHETHER GUESS/PROMPT IS CORRECT OR FALSE OR 'CLOSE'
+            print('[CLIENT] ' + user_input)  # This is to show on the server what the client's input was
+            print_serv_clie(f'\nWell you are not correct, try again.\n[ATTEMPTS REMAINING] {(_attempts - user_attempts)}')
     
     if failed:
-        send_to_client('The correct word was: ' + _word)
+        # BUG: the correct word isn't displayed on the client
+        print_serv_clie('The correct word was: ' + _word)
 
 # Socket Section -- Server
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-    server.bind((SERVER, PORT))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serv:
+    serv.bind((SERVER, PORT))
     print("[SERVER IS STARTING]")
-    server.listen()
+    serv.listen()
     print(f"[LISTENING] on {SERVER}")
-    conn, addr = server.accept()
+    conn, addr = serv.accept()
     
-    def send_to_client(text):
+    def print_serv_clie(text):
         data_to_send = text.encode(FORMAT)
-        server.send(data_to_send)
+        conn.send(data_to_send)
         print('[SERVER] ' + text)
 
     with conn:
         print(f"[NEW CONNECTION] {addr} connected")
+        greeting_text = (f'[CONNECTED] to {addr}').encode(FORMAT)
+        conn.send(greeting_text)
         pinoy_henyo_select()
-        
-
-        # while True:
-        #     msg = conn.recv(2048)
-        #     if not msg:
-        #         break
-
-            
-        #     conn.sendall(b'[DATA RECEIVED]')
-        
-        # connected = True
-        # while connected:
-        #     msg_length = conn.recv(HEADER).decode(FORMAT)
-        #     if msg_length:
-        #         msg_length = int(msg_length)
-        #         msg = conn.recv(msg_length).decode(FORMAT)
-        #         if msg == DISCONNECT_MESSAGE:
-        #             connected = False
-        #             print(f"[DISCONNECTED USER] {addr}")
-        #             break
-        #         print(f"[{addr}] {msg}")
-        #         conn.send("[MSG RECEIVED]".encode(FORMAT))
-        # conn.close()
-
-    # This was part of the video tutorial, which allows something to run wihtout 
-    # having to wait for another process. For our purposes this is not needed,
-    # but might be helpful for other added features down the line.
-    # 
-    # thread = threading.Thread(target=handle_client, args=(conn, addr))
-    # thread.start()
-    # print(f"[ACTIVE CONNECTIONS]: {threading.activeCount() - 1}") 
+    conn.close()
